36d337246247c56d05ce919d9989e2fb29d81e0a
翊纥 <1182238917@qq.com>
2026-04-15 14:29:03 +0800

    ctytest

--- src/main/java/com/secondclass/SecondClassBackendApplication.java
+++ src/main/java/com/secondclass/SecondClassBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // 💡 必须引入这个包
 
 @SpringBootApplication
-@EnableScheduling // 💡 给系统装上“电池”，开启定时任务引擎
+@EnableScheduling // 💡 给系统装上“电池”，开启定时任务引擎。
 public class SecondClassBackendApplication {
 
     public static void main(String[] args) {
